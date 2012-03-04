43c1e10b4913f6a7d22d5976d43973e6fc66f268
nanotone <nanotone@gmail.com>
2012-03-04 00:09:10 -0500

    cha-ching!

--- realtime.py
+++ realtime.py
@@ -1,3 +1,5 @@
+import json
+
 import tornado.ioloop
 import tornado.web
 import tornado.websocket
@@ -7,21 +9,30 @@ tornado.websocket.WebSocketHandler.allow_draft76 = (lambda self: True)
 clients = []
 current_status = 'stopped'
 
+def broadcast(msg):
+	msg = json.dumps(msg)
+	for c in clients:
+		c.write_message(msg)
+
 class MainHandler(tornado.web.RequestHandler):
 	def get(self):
 		global current_status
-		current_status = self.get_argument('status')
-		print "status arg:", current_status
-
-		for c in clients:
-			c.write_message(current_status)
+		if self.get_argument('action') == 'add':
+			broadcast({'type': 'addMiles',
+			           'miles': float(self.get_argument('miles')),
+			           'monies': float(self.get_argument('monies')),
+			         })
+		else:
+			current_status = self.get_argument('status')
+			print "status arg:", current_status
+			broadcast(current_status)
 		self.write("Hello, world")
 
 
 class WebSocketHandler(tornado.websocket.WebSocketHandler):
 	def open(self):
 		print "OPEN"
-		self.write_message(current_status)
+		#self.write_message(current_status)
 		clients.append(self)
 
 	def on_message(self, message):
